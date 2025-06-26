3d7a1d89b0d388f1ff86bc2b3319727a483e8ab5
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-26 19:44:07 +0700

    Added playGame function

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
 // Human choice function
 function getHumanChoice() {
     while (true) {
-        let choice = prompt("Choose between Rock, Paper, and Scissors => ");
+        let choice = prompt("Choose between Rock, Paper, and Scissors.");
         choice = choice.toLowerCase();
 
         if (choice === "rock" || choice === "paper" || choice === "scissors") {
@@ -25,32 +25,62 @@ function getHumanChoice() {
     }
 }
 
-// Variable
-let humanScore = 0;
-let computerScore = 0;
-
 // Play round function
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-
     let winner = "";
 
     if (humanChoice === computerChoice) {
-        winner = "Nobody";
+        alert(`Both chose ${humanChoice}. It's a tie!`);
+        winner = "Tie";
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper") 
     ) {
         winner = "You";
-        humanScore += 1;
     } else {
         winner = "Computer";
-        computerScore += 1;
     }
-
-    alert(`You picked ${humanChoice} and the computer ${computerChoice}. ${winner} won this round! The score is (You) ${humanScore} : ${computerScore} (Computer).`)
-
     return winner;
 }
+
+// Play game function
+function playGame() {
+    // Initialize score
+    let humanScore = 0;
+    let computerScore = 0;  
+
+    // Play 5 rounds
+    for (let round = 1; round <= 5; round++){
+        alert(`Round ${round} of 5`);
+
+        // Get choices for this round
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+
+        // Get the winner
+        const roundWinner = playRound(humanChoice, computerChoice);
+
+        // Get the score
+        if (roundWinner === "You") {
+            humanScore++;
+        } else if (roundWinner === "Computer") {
+            computerScore++;
+        }
+
+        alert(`Current score: You ${humanScore} - ${computerScore} Computer`);
+    }
+
+    // Determine final winner
+    if (humanScore > computerScore) {
+        alert(`You win the game! Final score: ${humanScore}-${computerScore}`);
+    } else if (computerScore > humanScore) {
+        alert(`Computer wins the game! Final score: ${humanScore}-${computerScore}`);
+    } else {
+        alert(`It's a tie! Final score: ${humanScore}-${computerScore}`);
+    }
+    
+}
+
+// Play
+playGame();

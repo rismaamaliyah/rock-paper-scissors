9943f2f1aad26e347f9e66bc52df90c2b5ba0dff
rismaamaliyah <rismaamaliyah2@gmail.com>
2025-06-26 17:53:34 +0700

    Added playRound function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Computer choice function
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
@@ -10,12 +11,39 @@ function getComputerChoice() {
     }
 }
 
+// Human choice function
 function getHumanChoice() {
     let choice = prompt("Choose between Rock, Paper, and Scissors.");
 
-    if (choice === "Rock" || choice === "Paper" || choice === "Scissors") {
-        return choice;
+    if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors") {
+        return choice.toLowerCase();
     } else {
         alert(`Please enter a valid choice`);
+    }
+}
+
+// Variable
+let humanScore = 0;
+let computerScore = 0;
+
+// Play round function
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    let comparison = humanChoice + "-" + computerChoice;
+    let winner = "";
+
+    switch (comparison) {
+        case "rock-scissors" || "paper-rock" || "scissors-paper":
+            winner = "You";
+            humanScore += 1;
+            break;
+        case "scissors-rock" || "rock-paper" || "paper-scissors":
+            winner = "Computer";
+            computerScore += 1;
+            break;
+        default:
+            winner = "Nobody";
     }
 }
